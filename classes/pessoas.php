ff60d87809bf3641c21345540427e6badb101437
Vinicius Carlos <vinicius.jcarlos@hotmail.com>
2021-12-18 14:15:38 -0300

    arquivo atualizado 14:15

--- classes/pessoas.php
+++ classes/pessoas.php
@@ -1,12 +1,56 @@
 <?php  
 
-    class Pessoas {
+   /*
+   //exemplo 1 
+   class Pessoas{
+     
+    public $nome, $apelido, $idade;
 
-     public $nomes, $apelido, $idade;
+   }
+   // fim exemplo 1
+   */
 
+   // exemplo 2 - utiiza o set para tratar e retornar uma informacao 
+   /*
 
-        
+    class Pessoas{
+
+     public $nome, $apelido, $idade;
+
+     public function setNome($nome){
+
+        $this->nome = strtolower($nome);
+     }
+
 
     }
+     // fim exemplo 2
+
+     */
+
+     // exemplo 3 get usado para recuperar as informacoes digitadas, controla a forma que devolvemos os dados
+
+     class Pessoas{
+
+        public $nome, $apelido, $idade;
+   
+        public function setNome($nome){
+   
+           $this->nome = strtolower($nome);
+        }
+
+        public function getNome(){
+
+            return ucwords($this->nome);
+
+        }
+   
+   
+       }
+
+    //fim exemplo3
+
+
+
 
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,59 @@
 <?php
 
     require_once("classes/pessoas.php");
-    
+   
+    /*
+    //exemplo 1
     $pessoa1 = new Pessoas;
+   
+    $pessoa1->nome = "Vinicius";
+    $pessoa1->apelido = "Vinny";
+    $pessoa1->idade = "37 anos";
 
-    var_dump($pessoa1);
+    $pessoa2 = new Pessoas;
 
-     
+    $pessoa2->nome = "Alana";
+    $pessoa2->apelido = "Nana";
+    $pessoa2->idade = "29 anos";
+
+
+    echo "O nome da primeira pessoa é: ".$pessoa1->nome;
+    echo "<br>";
+    echo "O nome da segunda pessoa é: ".$pessoa2->nome;
+
+    //var_dump($pessoa1);
+    //fim exemplo 1
+    */
+    
+    /*
+    //exemplo 2 utilizando o set
+    $pessoa1 = new Pessoas;
+    
+    $pessoa1->setNome("Vinicius Carlos");
+   // $pessoa1->apelido = "Vinny";
+   // $pessoa1->idade = "37 anos";
+    
+    
+
+    echo  "O nome da primeira pessoa é: ".$pessoa1->nome;
+    echo "<br>";
+
+    //fim exemplo 2
+    */
+
+    //exemplo3 utilizando o get para o retorno diferente do original usando o metodo set na saida do resultado
+
+    $pessoa1 = new Pessoas;
+    
+    $pessoa1->setNome("Vinicius carlos");
+    $pessoa1->apelido = "Vinny";
+    $pessoa1->idade = "37 anos";
+    
+    
+
+    echo  "O nome da primeira pessoa é: ".$pessoa1->getNome();
+    echo "<br>";
+
+
+    // fim exemplo 3
 ?>
