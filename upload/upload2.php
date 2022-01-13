e0576de650593f9a1294a02f6629d7ca76168ef6
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2022-01-13 10:33:36 -0300

    trabalhando com upload imagem 4

--- upload/upload2.php
+++ upload/upload2.php
@@ -1,12 +1,7 @@
 <?php
 
     include ('classes/Upload.php');
-
-<<<<<<< HEAD
-  echo Upload::subir($_FILES['arq'],1048576,['image/gif', 'image/png', 'image/jpeg', 'image/pjpeg']);
-=======
-  echo Upload::subir($_FILES['arq'],100,['image/gif', 'image/png', 'image/jpeg', 'image/pjpeg']);
->>>>>>> b2df7e72e5759e3454f90a56eef562cb744ab927
+    echo Upload::subir($_FILES['arq'],1048576,['image/gif', 'image/png', 'image/jpeg', 'image/pjpeg']);
 
 
 ?>
